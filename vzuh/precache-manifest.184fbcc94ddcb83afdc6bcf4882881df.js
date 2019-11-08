623298e864c496c5d319b2d1c5b6c81154484d14
mestima <mestima@icloud.com>
2019-11-08 23:36:39 +0300

    vzuh lang build upload

---
+++ vzuh/precache-manifest.184fbcc94ddcb83afdc6bcf4882881df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d8b1f5462e5ad406310dc99d0d95032",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d963de34fb5fc04c2d2e",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "1697a8b69a2370ad62c6",
+    "url": "/static/css/main.c78f2416.chunk.css"
+  },
+  {
+    "revision": "d963de34fb5fc04c2d2e",
+    "url": "/static/js/2.5b4a4664.chunk.js"
+  },
+  {
+    "revision": "1697a8b69a2370ad62c6",
+    "url": "/static/js/main.2027e594.chunk.js"
+  },
+  {
+    "revision": "563b41250634a03114f8",
+    "url": "/static/js/runtime-main.a55650d1.js"
+  }
+]);
